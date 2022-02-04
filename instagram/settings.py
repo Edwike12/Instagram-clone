fb18bbda649eb208d2b06d3b4a5035fe5ae3ad45
Edwike12 <nyaunchoedwike@gmail.com>
2022-02-04 23:10:42 +0300

    installed python-decouple and imported config from decouple in settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config,Csv
+from decouple import config, Csv
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myinsta.app.MyinstaConfig',
+    'myinsta',
     'bootstrap4',
     'cloudinary',
 ]
@@ -140,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
