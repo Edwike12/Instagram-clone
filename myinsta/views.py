b5f21ca054c097e01d93aab892972fb373ace1cd
Edwike12 <nyaunchoedwike@gmail.com>
2022-02-05 00:50:05 +0300

    created a view function for my page rendered from index.html

--- myinsta/views.py
+++ myinsta/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html',)
