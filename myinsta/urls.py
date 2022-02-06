3314ec7e07a60c426ac1b9738966cb5e758c39f1
Edwike12 <nyaunchoedwike@gmail.com>
2022-02-06 18:42:38 +0300

    added a url link for profile, update profile, add post and comment sections

--- myinsta/urls.py
+++ myinsta/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns=[
    path('',views.index,name = 'index'),
+   path('profile/', views.profile, name='profile'),
+   path('update_profile/<int:id>', views.update_profile, name='update_profile'),
+   path('add_post/', views.add_post, name='add_post'),
+   path('comments/<post_id>', views.comments, name='comments'),
    
 ]
